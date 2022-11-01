5db41664c365c64a43746de9ad63c1998951df74
arokhsaz3 <arokhsaz3@gatech.edu>
2022-10-31 23:37:49 -0400

    volume

--- speaker.py
+++ speaker.py
@@ -4,5 +4,7 @@ import pygame
 pygame.mixer.init()
 pygame.mixer.music.load("name.wav")
 pygame.mixer.music.play()
+pygame.mixer.music.set_volume(1.0)
+
 while pygame.mixer.music.get_busy() == True:
     continue
