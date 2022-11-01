3d064d6be125b59fc95293e2c70cab349a932989
arokhsaz3 <arokhsaz3@gatech.edu>
2022-11-01 15:11:14 -0400

    Larger state control

--- volume.py
+++ volume.py
@@ -5,7 +5,7 @@ import string
 import pygame
 
 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 def parse_int(str):
     return int(str.strip(string.ascii_letters))
@@ -18,14 +18,15 @@ pygame.mixer.music.load("name.wav")
 pygame.mixer.music.play()
 
 while True:
+    if pygame.mixer.music.get_busy() == False:
+        pygame.mixer.music.load("name.wav")
+        pygame.mixer.music.play()
+
     line = ser.readline()
     if len(line) == 0:
         print("out")
     volume = parse_int(line.decode("utf-8"))
     print(volume)
     pygame.mixer.music.set_volume(volume/100)
-    if pygame.mixer.music.get_busy() == False:
-        pygame.mixer.music.load("name.wav")
-        pygame.mixer.music.play()
         
 
